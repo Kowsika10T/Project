4a50169ce09450df7fd5d91bdac199a72799d1c1
Kowsika10T <113577034+Kowsika10T@users.noreply.github.com>
2023-11-24 18:54:03 +0530

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,23 @@
+<?php
+$conn = new mysqli('localhost','root','','login-reg');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Login successful!";
+    } else {
+        echo "Login failed. Invalid username or password.";
+    }
+}
+
+
+$conn->close();
+?>
